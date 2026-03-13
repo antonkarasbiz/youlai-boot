e441d394f64025cf7c768de70af2b276e47801aa
antonkarasbiz <antonkarasbiz@gmail.com>
2026-03-13 20:39:57 +0800

    feat: 添加数据权限获取方法及更新配置

--- src/main/java/com/youlai/boot/security/util/SecurityUtils.java
+++ src/main/java/com/youlai/boot/security/util/SecurityUtils.java
@@ -4,6 +4,7 @@ import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.util.StrUtil;
 import com.youlai.boot.common.constant.SecurityConstants;
 import com.youlai.boot.common.constant.SystemConstants;
+import com.youlai.boot.security.model.RoleDataScope;
 import com.youlai.boot.security.model.SysUserDetails;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpHeaders;
@@ -68,6 +69,15 @@ public class SecurityUtils {
      */
     public static Long getDeptId() {
         return getUser().map(SysUserDetails::getDeptId).orElse(null);
+    }
+
+    /**
+     * 获取数据权限列表
+     *
+     * @return 数据权限列表
+     */
+    public static List<RoleDataScope> getDataScopes() {
+        return getUser().map(SysUserDetails::getDataScopes).orElse(List.of());
     }
 
     /**
